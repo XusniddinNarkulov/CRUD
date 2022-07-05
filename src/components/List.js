3f35cccd093846d68882c1d17130518612579f63
Xusniddin <narkulof@gmail.com>
2022-07-05 14:30:35 +0500

    fixed

--- src/components/List.js
+++ src/components/List.js
@@ -33,68 +33,21 @@ const List = (props) => {
                      style={{ width: "100%" }}
                   >
                      <input type="checkbox" />
-                     {/* {edit ? (
-                     <span className="input" contentEditable>
-                        {task}
-                     </span>
-                  ) : (
-                     <span
-                        className="input"
-                        onClick={(e) => {
-                           console.log(e);
-                           editFunc;
-                        }}
-                     >
-                        {task}
-                     </span>
-                  )} */}
-                     {/* <span
-                        className="input"
-                        onClick={(e) => {
-                           console.log(e);
-                           e.target.setAttribute("contentEditable", "");
-                        }}
-                        onChange={(e) => {
-                           console.log(e);
-                           setEdit(e.target.innerHTML);
-                        }}
-                     >
-                        {task}
-                     </span> */}
+
                      <input
                         type="text"
-                        // disabled
                         className="input"
                         style={{
                            width: "80%",
                         }}
                         defaultValue={task}
-                        onClick={(e) => {
-                           console.log(e);
-                           // e.target.setAttribute("contentEditable", "");
-                        }}
                         onChange={(e) => {
                            setEdit(e.target.value);
                         }}
                      />
-
-                     {/* <button
-                        type="button"
-                        className="edit_task_button "
-                        onClick={(e) => {
-                           console.log(e);
-                           e.target.previousElementSibling.setAttribute(
-                              "contentEditable",
-                              ""
-                           );
-                        }}
-                     >
-                        <i className="fa fa-pencil" aria-hidden="true"></i>
-                     </button> */}
-                     <button type="submit"></button>
                   </form>
+
                   <button
-                     // type="submit"
                      className="delete"
                      onClick={() => {
                         removeTaskAction(id);
